f7d627e7163e012b42a0dd4e241a8a395e45899b
sakthiveltsmr <sakthiveltsmr@gmail.com>
2022-03-10 18:12:08 +0530

    app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Navigate,
+} from "react-router-dom";
+import "./App.css";
 
+import ForgotPass from "./component/forgotpass/forgotpass";
+import Login from "./component/login/login";
+import Register from "./component/register/register";
+import Protected from "./component/protected/protected";
+import ChangePass from "./component/changepass/changepass";
+
+export const AppContext = React.createContext();
 function App() {
+  const [log, setlog] = useState(false);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Routes>
+          <AppContext.Provider value={[log, setlog]}>
+            <Route path="/protected" element={<Protected />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+            <Route path="/forgotpassword" element={<ForgotPass />} />
+            <Route path="/resetpassword/:id/:token" element={<ChangePass />} />
+            <Route path="/">
+              <Navigate to="/login" />
+            </Route>
+          </AppContext.Provider>
+        </Routes>
+      </Router>
     </div>
   );
 }
